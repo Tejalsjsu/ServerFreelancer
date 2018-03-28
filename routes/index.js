033daa2d19730f9f624b659985c17944499020a6
tejalpadharia <36251006+tejalpadharia@users.noreply.github.com>
2018-03-28 00:44:04 -0700

    MongoDB
    Added MongoDb connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var mongodb = require('mongodb');
+var monk = require ('monk');
+//var db = monk('mongodb://localhost:27017');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/ GET Userlist page. /
+router.get('/userlist', function(req, res) {
+    var MongoClient = mongodb.MongoClient;
+    var url = "mongodb://localhost:27017/";
+    MongoClient.connect(url, function (err,db) {
+        if(err){
+            console.log(err.toString()+ " " +url);
+        }
+        else{
+            console.log("Connection established");
+        }
+        var dbo = db.db("freelancer");
+        //var collection = db.collection("login");
+        dbo.collection("login").find({}).toArray(function(err,result){
+            if(err){
+                console.log("Error in getting result");
+            }else if(result.length){
+            res.json({'userlist': result});
+            }else{
+                console.log('No documents found');
+            }
+        });
+    })
+
 });
 
 
