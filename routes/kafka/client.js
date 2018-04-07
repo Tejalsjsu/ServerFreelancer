f259a4f1362b74e7bd7fcd15048ebba05249e08e
tejalpadharia <36251006+tejalpadharia@users.noreply.github.com>
2018-04-07 00:58:37 -0700

    Added code for kafka calla
    Added code for kafka calla

--- routes/kafka/client.js
+++ routes/kafka/client.js
@@ -2,8 +2,8 @@ var rpc = new (require('./kafkarpc'))();
 
 //make request to kafka
 function make_request(queue_name, msg_payload, callback){
-    console.log('in make request');
-    console.log("User Id in client : " +msg_payload.employerId);
+    console.log('in make request client');
+    //console.log("User Id is in client : " +msg_payload.employerId);
     rpc.makeRequest(queue_name, msg_payload, function(err, response){
 
         if(err)
